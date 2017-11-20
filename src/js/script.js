04e88b61cc529d5c1bbe86b825d6b4a79e697e5a
piterkm <kowalowka.malkowski@gmail.com>
2017-11-20 17:14:50 +0100

    Problem z uruchomieniem Masonry
    Nie sypie już błędem, jednak masonry w ogóle nie działa, czyli nie tworzy grida.

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("działam");
+
+    //TWORZENIE GRIDA (MASONRY)
+    // var container = document.querySelector('#masonry');
+    // var masonry = new Masonry(container, {
+    //   columnWidth: 399,
+    //   itemSelector: '.secondary-element'
+    // });
+
+    $('#masonry').masonry({
+        itemSelector: '.secondary-element',
+        columnWidth: 380,
+    })
+    console.log("tworzę grida o szerokości kolumny 380px");
+
+
+});
