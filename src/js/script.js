f367a176821b8250cf20cf7605735d5ac7211455
piterkm <kowalowka.malkowski@gmail.com>
2017-11-28 18:03:07 +0100

    Poprawienie górnej nawigacji

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,10 @@ document.addEventListener("DOMContentLoaded", function(){
     console.log("działam");
 
     //TWORZENIE GRIDA (MASONRY)
-    // var container = document.querySelector('#masonry');
-    // var masonry = new Masonry(container, {
-    //   columnWidth: 399,
-    //   itemSelector: '.secondary-element'
-    // });
-
     $('#masonry').masonry({
         itemSelector: '.secondary-element',
         columnWidth: 398,
     })
     console.log("tworzę grida o szerokości kolumny 380px");
-
 
 });
